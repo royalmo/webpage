4e30259340f866d2c0aaa859dfea7dd50461e963
royalmo <royalmo6@gmail.com>
2020-08-18 17:16:32 +0200

    Idk if this will work :(

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-    include(“home.html”);
-    echo("This website isn't ready yet!");
-?>
+<html>
+    <head>
+        <title>hello</title>
+    </head>
+    <body>
+        <h1>Test</h1>
+        
+        <?php
+            include(“home.html”);
+            echo("This website isn't ready yet!");
+        ?>
+    </body>
+</html>
+
