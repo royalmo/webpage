f99cbba2ec6a1cffda0d96c4f6c1c40947eed5bb
royalmo <royalmo6@gmail.com>
2020-08-26 10:27:36 +0200

    Updating include_once, changing composer

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<html>
-    <head>
-        <title>hello</title>
-    </head>
-    <body>
-        <h1>Test</h1>
-        
-        <?php
-            include(“home.html”);
-            echo("This website isn't ready yet!");
-        ?>
-    </body>
-</html>
-
+<?php include_once("home.html"); ?>
